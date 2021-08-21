483e1c54d8d4c8e727314586902167f41cb6bbfa
LEDgEr67 <smt786.sm67@gmail.com>
2021-08-21 16:52:53 +0530

    duplicate stories

--- src/components/stories/index.js
+++ src/components/stories/index.js
@@ -3,9 +3,20 @@ import StoryPreview from "./story-preview";
 export default function Stories(props) {
   return (
     <div className="rounded w-full h-28 bg-white border-gray-primary mb-6 border">
-      <span className="flex pt-2 pb-2">
-        <StoryPreview username="none" />
-      </span>
+      <ul className="flex pt-2 pb-2 overflow-x-scroll">
+        <li>
+          <StoryPreview username="none" />
+        </li>
+        <li>
+          <StoryPreview username="none" />
+        </li>
+        <li>
+          <StoryPreview username="none" />
+        </li>
+        <li>
+          <StoryPreview username="none" />
+        </li>
+      </ul>
     </div>
   );
 }
