5bb4e2ea441b4a2aab5b7e88f7a98c2217e6b3f6
LEDgEr67 <smt786.sm67@gmail.com>
2021-08-30 11:03:08 +0530

    profile-image commented for deployment fix

--- src/helpers/profile-image.js
+++ src/helpers/profile-image.js
@@ -1,41 +1,41 @@
-// import { pathExists } from "path-exists";
-import fs from "fs";
+// // import { pathExists } from "path-exists";
+// import fs from "fs";
 
-export default function ProfileImage({ fileName, className }) {
-  const path = `/images/avatars/${fileName}.jpg`;
-  // try {
-  //   if (path.exists()) {
-  //     console.log("thoufic it is there");
-  //   } else {
-  //     console.log("thoufic it is not there");
-  //   }
-  // } catch (err) {
-  //   console.log("thoufic", err);
-  // }
+// export default function ProfileImage({ fileName, className }) {
+//   const path = `/images/avatars/${fileName}.jpg`;
+//   // try {
+//   //   if (path.exists()) {
+//   //     console.log("thoufic it is there");
+//   //   } else {
+//   //     console.log("thoufic it is not there");
+//   //   }
+//   // } catch (err) {
+//   //   console.log("thoufic", err);
+//   // }
 
-  // try {
-  //   if (fs.existsSync(path)) {
-  //     //file exists
-  //     console.log("thoufic it is there");
-  //   }
-  // } catch (err) {
-  //   console.error("thoufic", err);
-  // }
-  return (
-    <>
-      {/* isFilePresent ? (
-      <img
-        className={className}
-        src={`/images/avatars/${fileName}.jpg`}
-        alt={`${fileName}`}
-      ></img>
-      ) : ( */}
-      <img
-        className={className}
-        src={`/images/avatars/default.png`}
-        alt={`default `}
-      ></img>
-      {/* )} */}
-    </>
-  );
-}
+//   // try {
+//   //   if (fs.existsSync(path)) {
+//   //     //file exists
+//   //     console.log("thoufic it is there");
+//   //   }
+//   // } catch (err) {
+//   //   console.error("thoufic", err);
+//   // }
+//   return (
+//     <>
+//       {/* isFilePresent ? (
+//       <img
+//         className={className}
+//         src={`/images/avatars/${fileName}.jpg`}
+//         alt={`${fileName}`}
+//       ></img>
+//       ) : ( */}
+//       <img
+//         className={className}
+//         src={`/images/avatars/default.png`}
+//         alt={`default `}
+//       ></img>
+//       {/* )} */}
+//     </>
+//   );
+// }
